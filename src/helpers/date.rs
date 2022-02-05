66317dfb9e255e7f63e5de4c76340c1cd42932e5
pmagaz <magazpablo@gmail.com>
2022-02-05 19:11:11 +0100

    fix: update date helper

--- src/helpers/date.rs
+++ src/helpers/date.rs
@@ -2,6 +2,6 @@ pub use chrono::offset::{Local, TimeZone};
 pub use chrono::{Date, DateTime, Duration, NaiveDateTime, NaiveTime, Utc};
 
 pub fn parse_time(date: i64) -> DateTime<Local> {
-  let ts = chrono::NaiveDateTime::from_timestamp(date, 0);
-  Local.from_local_datetime(&ts).unwrap()
+    let ts = chrono::NaiveDateTime::from_timestamp(date, 0);
+    Local.from_local_datetime(&ts).unwrap()
 }
