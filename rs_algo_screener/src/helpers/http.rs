e2a79f4584514aa3ed43982c9b28365f0bcfd897
pmagaz <magazpablo@gmail.com>
2022-02-20 14:18:56 +0100

    chore: add fix dependencies versioning

--- rs_algo_screener/src/helpers/http.rs
+++ rs_algo_screener/src/helpers/http.rs
@@ -4,17 +4,17 @@ use std::fmt::Debug;
 
 pub async fn request<T>(url: &str, data: T) -> Result<Response, Error>
 where
-  for<'de> T: Serialize + Deserialize<'de> + Debug,
-  //for<'de> R: Serialize + Deserialize<'de>,
+    for<'de> T: Serialize + Deserialize<'de> + Debug,
+    //for<'de> R: Serialize + Deserialize<'de>,
 {
-  println!("[Request] to {:?} with {:?} ", url, &data);
+    println!("[Request] to {:?} with {:?} ", url, &data);
 
-  Client::builder()
-    .build()?
-    .get(url)
-    //.basic_auth(gh_user.clone(), Some(gh_pass.clone()))
-    //.form(&data)
-    //.json(&data)
-    .send()
-    .await
+    Client::builder()
+        .build()?
+        .get(url)
+        //.basic_auth(gh_user.clone(), Some(gh_pass.clone()))
+        //.form(&data)
+        //.json(&data)
+        .send()
+        .await
 }
