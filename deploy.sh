03b6ea8093f309d957ebf88d9e42a4af893845a9
pmagaz <magazpablo@gmail.com>
2022-03-05 18:50:44 +0100

    build: move docker login

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,17 @@ do
     case $opt in
         "all")
             echo "Deploying: $opt";
-            docker build -t pmagaz/rs-algo-backend:latest rs_algo_backend ; docker push pmagaz/rs-algo-backend:latest\ ; docker build -t pmagaz/rs-algo-screener:latest rs_algo_screener ; docker push pmagaz/rs-algo-screener:latest\ ; ansible-playbook playbook.yml  
+            docker login ; docker build -t pmagaz/rs-algo-backend:latest rs_algo_backend ; docker push pmagaz/rs-algo-backend:latest ; docker build -t pmagaz/rs-algo-screener:latest rs_algo_screener ; docker push pmagaz/rs-algo-screener:latest ; ansible-playbook playbook.yml  
             break
             ;;
         "rs-algo-backend")
             echo "Deploying: $opt";
-            docker build -t pmagaz/rs-algo-backend:latest rs_algo_backend ; docker push pmagaz/rs-algo-backend:latest\ ; ansible-playbook playbook.yml 
+            docker login ;  docker build -t pmagaz/rs-algo-backend:latest rs_algo_backend ; docker push pmagaz/rs-algo-backend:latest ; ansible-playbook playbook.yml 
             break
             ;;
         "rs-algo-screener")
             echo "Deploying: $opt";
-            docker build -t pmagaz/rs-algo-screener:latest rs_algo_screener ; docker push pmagaz/rs-algo-screener:latest\ ; ansible-playbook playbook.yml 
+            docker login ; docker build -t pmagaz/rs-algo-screener:latest rs_algo_screener ; docker push pmagaz/rs-algo-screener:latest ; ansible-playbook playbook.yml 
             break
             ;;
         "Quit")
