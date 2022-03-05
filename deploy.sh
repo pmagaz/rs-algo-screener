1b552d2d26cc75e569364b9ae55f83e4f0a93d01
pmagaz <magazpablo@gmail.com>
2022-03-05 21:22:31 +0100

    build: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 PS3='Please enter your choice: '
-options=("all" "rs-algo-backend" "rs-algo-screener" "Quit")
+options=("build & deploy all" "deploy all" "build & deploy rs-algo-backend" "build & deploy rs-algo-screener" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "all")
+        "build & deploy all")
             echo "Deploying: $opt";
             docker login ; docker build -t pmagaz/rs-algo-backend:latest rs_algo_backend ; docker push pmagaz/rs-algo-backend:latest ; docker build -t pmagaz/rs-algo-screener:latest rs_algo_screener ; docker push pmagaz/rs-algo-screener:latest ; ansible-playbook playbook.yml  
             break
             ;;
-        "rs-algo-backend")
+        "deploy all")
+            echo "Deploying: $opt";
+            ansible-playbook playbook.yml
+            break
+            ;;
+        "build & deploy rs-algo-backend")
             echo "Deploying: $opt";
             docker login ;  docker build -t pmagaz/rs-algo-backend:latest rs_algo_backend ; docker push pmagaz/rs-algo-backend:latest ; ansible-playbook playbook.yml 
             break
             ;;
-        "rs-algo-screener")
+        "build & deploy rs-algo-screener")
             echo "Deploying: $opt";
             docker login ; docker build -t pmagaz/rs-algo-screener:latest rs_algo_screener ; docker push pmagaz/rs-algo-screener:latest ; ansible-playbook playbook.yml 
             break
