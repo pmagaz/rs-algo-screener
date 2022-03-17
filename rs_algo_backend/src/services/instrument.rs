cfddec720d948478995b10add11a131f8f24925a
pmagaz <magazpablo@gmail.com>
2022-03-17 19:28:04 +0100

    chore: fix fonts?

--- rs_algo_backend/src/services/instrument.rs
+++ rs_algo_backend/src/services/instrument.rs
@@ -6,37 +6,77 @@ use crate::models::app_state::AppState;
 use crate::models::Instrument;
 use std::time::Instant;
 
+use actix_files as fs;
+use actix_web::http::header::{ContentDisposition, DispositionType};
+use actix_web::{get, App, Error, HttpRequest, HttpServer};
 use actix_web::{web, HttpResponse};
 use rs_algo_shared::helpers::date::Local;
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::path::PathBuf;
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub struct SymbolQuery {
     pub symbol: String,
 }
 
-pub async fn render(
+pub async fn find_one(
     query: web::Query<SymbolQuery>,
     state: web::Data<AppState>,
 ) -> Result<HttpResponse, RsAlgoError> {
     let now = Instant::now();
+    let symbol = &query.symbol;
 
-    let instrument = db::instrument::find_by_symbol(&query.symbol, &state)
+    let instrument = db::instrument::find_by_symbol(symbol, &state)
         .await
         .unwrap()
         .unwrap();
-    let backend = Backend::new();
-    let _output = backend.render(&instrument);
 
     println!(
-        "[RENDER] {:?} {:?} {:?}",
+        "[FINDONE] {:?} {:?} {:?}",
         instrument.symbol,
         Local::now(),
         now.elapsed()
     );
 
     Ok(HttpResponse::Ok().json(instrument))
+}
+
+pub async fn render(
+    query: web::Query<SymbolQuery>,
+    state: web::Data<AppState>,
+) -> Result<fs::NamedFile, RsAlgoError> {
+    let now = Instant::now();
+    let symbol = &query.symbol;
+
+    let instrument = db::instrument::find_by_symbol(symbol, &state)
+        .await
+        .unwrap()
+        .unwrap();
+
+    let backend = Backend::new();
+    let _output = backend.render(&instrument);
+
+    let image_folder = &env::var("BACKEND_PLOTTER_OUTPUT_FOLDER").unwrap();
+    let mut image_path = PathBuf::new();
+    image_path.push(image_folder);
+    image_path.push(symbol);
+    image_path.push(".png");
+    let file = fs::NamedFile::open(image_path).unwrap();
+
+    println!(
+        "[RENDER] {:?} {:?} {:?}",
+        symbol,
+        Local::now(),
+        now.elapsed()
+    );
+
+    Ok(
+        file.use_last_modified(true), // .set_content_disposition(ContentDisposition {
+                                      //     disposition: DispositionType::Attachment,
+                                      //     parameters: vec![],
+                                      // })
+    )
 }
 
 pub async fn find(params: String, state: web::Data<AppState>) -> Result<HttpResponse, RsAlgoError> {
