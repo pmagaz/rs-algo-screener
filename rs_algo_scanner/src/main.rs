6e5c26f704294f9838af4c41d94707fbb7f55645
Pablo <magazpablo@gmail.com>
2022-03-28 09:42:43 +0200

    fix: fix wrong bullish gap calculation

--- rs_algo_scanner/src/main.rs
+++ rs_algo_scanner/src/main.rs
@@ -56,7 +56,7 @@ async fn main() -> Result<()> {
     let symbols = screener.get_symbols().await.unwrap().symbols;
 
     // let symbols = [Symbol {
-    //     symbol: "DOCU.US_4".to_owned(),
+    //     symbol: "FLEX.US_9".to_owned(),
     //     category: "".to_owned(),
     //     description: "".to_owned(),
     //     currency: "".to_owned(),
