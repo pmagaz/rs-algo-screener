ce5bbbf07d42d77fda9219f538ceee1a19061af7
Pablo <magazpablo@gmail.com>
2022-04-01 20:46:42 +0200

    chore: update general strategy params

--- rs_algo_backend/src/strategies/general.rs
+++ rs_algo_backend/src/strategies/general.rs
@@ -33,13 +33,13 @@ impl General {
                  {"current_candle": "Karakasa"},
                  {"$expr": {"$gte": ["$indicators.ema_a.current_a","$indicators.ema_b.current_a"]}},
                  {"$expr": {"$gte": ["$indicators.ema_b.current_a","$indicators.ema_c.current_a"]}},
-                 //{"indicators.stoch.current_a":  {"$lt": stoch_bottom }},
+                 {"indicators.stoch.current_a":  {"$lt": stoch_bottom }},
                ]},
                 {"$and": [
                  {"current_candle": "MorningStar"},
                  {"$expr": {"$gte": ["$indicators.ema_a.current_a","$indicators.ema_b.current_a"]}},
                  {"$expr": {"$gte": ["$indicators.ema_b.current_a","$indicators.ema_c.current_a"]}},
-                 //{"indicators.stoch.current_a":  {"$lt": stoch_bottom }},
+                 {"indicators.stoch.current_a":  {"$lt": stoch_bottom }},
                ]},
                 {"$and": [
                  {"current_candle": "BullishGap"},
