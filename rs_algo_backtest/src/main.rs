89c421a919a99880104487c447e35dd1b58bd216
Pablo <magazpablo@gmail.com>
2022-04-30 18:58:02 +0200

    ops: update deployment

--- rs_algo_backtest/src/main.rs
+++ rs_algo_backtest/src/main.rs
@@ -3,7 +3,8 @@ use std::time::Instant;
 use rs_algo_shared::error::{Result, RsAlgoError};
 use rs_algo_shared::helpers::date;
 use rs_algo_shared::helpers::date::Local;
-use rs_algo_shared::models::Instrument;
+use rs_algo_shared::models::backtest_instrument::BackTestInstrument;
+use rs_algo_shared::models::instrument::Instrument;
 
 use dotenv::dotenv;
 use rs_algo_shared::helpers::http::{request, HttpMethod};
@@ -11,10 +12,13 @@ use rs_algo_shared::helpers::http::{request, HttpMethod};
 use std::env;
 use std::{thread, time};
 
+mod back_test;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
     let start = Instant::now();
+
     let endpoint = env::var("BACKEND_BACKTEST_INSTRUMENTS_ENDPOINT")
         .unwrap()
         .clone();
@@ -26,7 +30,7 @@ async fn main() -> Result<()> {
         .await
         .unwrap();
 
-    println!("[RES] {:?}", instruments);
+    back_test::backtest(&instruments);
     println!("[Finished] at {:?}  in {:?}", Local::now(), start.elapsed());
 
     Ok(())
