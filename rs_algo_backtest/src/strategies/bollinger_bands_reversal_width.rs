70df5c414d49c6ebca66c92fc2b4a1e4a80572c3
Pablo <magazpablo@gmail.com>
2022-06-13 10:23:38 +0200

    chore: add bbw calculation

---
+++ rs_algo_backtest/src/strategies/bollinger_bands_reversal_width.rs
@@ -0,0 +1,113 @@
+use super::strategy::Strategy;
+
+use crate::helpers::calc::*;
+use crate::trade::*;
+use async_trait::async_trait;
+use rs_algo_shared::error::Result;
+use rs_algo_shared::models::backtest_instrument::*;
+use rs_algo_shared::models::candle::*;
+use rs_algo_shared::models::instrument::Instrument;
+use rs_algo_shared::models::pattern::*;
+
+pub struct BollingerBands<'a> {
+    name: &'a str,
+}
+
+#[async_trait]
+impl<'a> Strategy for BollingerBands<'a> {
+    fn new() -> Result<Self> {
+        Ok(Self {
+            name: "Bollinger_Bands_Reversal_Riding",
+        })
+    }
+
+    fn name(&self) -> &str {
+        self.name
+    }
+
+    fn market_in_fn(&self, index: usize, instrument: &Instrument, stop_loss: f64) -> TradeResult {
+        let prev_index = get_prev_index(index);
+
+        let close_price = &instrument.data.get(index).unwrap().close;
+        let prev_close = &instrument.data.get(prev_index).unwrap().close;
+
+        let patterns = &instrument.patterns.local_patterns;
+        let current_pattern = get_current_pattern(index, patterns);
+
+        let low_band = instrument.indicators.bb.data_b.get(index).unwrap();
+        let prev_low_band = instrument.indicators.bb.data_b.get(prev_index).unwrap();
+
+        let entry_condition = current_pattern != PatternType::ChannelDown
+            && current_pattern != PatternType::LowerHighsLowerLows
+            && close_price < low_band
+            && prev_close >= prev_low_band;
+
+        resolve_trade_in(index, instrument, entry_condition, stop_loss)
+    }
+
+    fn market_out_fn(
+        &self,
+        index: usize,
+        instrument: &Instrument,
+        trade_in: &TradeIn,
+    ) -> TradeResult {
+        let prev_index = get_prev_index(index);
+        let candle_type = &instrument.data.get(index).unwrap().candle_type;
+
+        let top_band = instrument.indicators.bb.data_a.get(index).unwrap();
+        let mid_band = instrument.indicators.bb.data_c.get(index).unwrap();
+        let low_band = instrument.indicators.bb.data_b.get(index).unwrap();
+
+        let prev_top_band = instrument.indicators.bb.data_a.get(prev_index).unwrap();
+
+        let patterns = &instrument.patterns.local_patterns;
+        let current_pattern = get_current_pattern(index, patterns);
+        let close_price = &instrument.data.get(index).unwrap().close;
+        let prev_close = &instrument.data.get(prev_index).unwrap().close;
+
+        let backwards_candles = 5;
+        let max_band_hits = 3;
+        let mut hits_over_top_band: usize = 0;
+        let mut hits_over_low_band: usize = 0;
+        let mut hits_above_mid_band: usize = 0;
+
+        for x in (index - backwards_candles..index).rev() {
+            let highest_price = instrument.data.get(x).unwrap().high;
+            if highest_price > *top_band {
+                hits_over_top_band += 1;
+            }
+
+            let mid_price = instrument.data.get(x).unwrap().close;
+            if mid_price < *mid_band {
+                hits_above_mid_band += 1;
+            }
+
+            let lowest_price = instrument.data.get(x).unwrap().low;
+            if lowest_price < *low_band {
+                hits_over_low_band += 1;
+            }
+        }
+
+        let exit_condition = (current_pattern != PatternType::ChannelUp
+            && current_pattern != PatternType::HigherHighsHigherLows
+            && (hits_over_top_band <= 5 && hits_above_mid_band > 5))
+            //&& (close_price > top_band && prev_close <= prev_top_band ))
+            || (hits_over_low_band >= 3 );
+
+        let stop_loss = true;
+        resolve_trade_out(index, instrument, trade_in, exit_condition, stop_loss)
+    }
+
+    fn backtest_result(
+        &self,
+        instrument: &Instrument,
+        trades_in: Vec<TradeIn>,
+        trades_out: Vec<TradeOut>,
+        equity: f64,
+        commission: f64,
+    ) -> BackTestResult {
+        resolve_backtest(
+            instrument, trades_in, trades_out, self.name, equity, commission,
+        )
+    }
+}
---
+++ rs_algo_scanner/src/indicators/bbw.rs
@@ -0,0 +1,66 @@
+use super::Indicator;
+use crate::error::Result;
+
+use serde::{Deserialize, Serialize};
+use ta::indicators::{BollingerBands, BollingerBandsOutput};
+use ta::Next;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BollingerBW {
+    bb: BollingerBands,
+    data_a: Vec<f64>,
+    data_b: Vec<f64>,
+    data_c: Vec<f64>,
+}
+
+impl Indicator for BollingerBW {
+    fn new() -> Result<Self> {
+        Ok(Self {
+            bb: BollingerBands::new(20, 2.0).unwrap(),
+            data_a: vec![],
+            data_b: vec![],
+            data_c: vec![],
+        })
+    }
+
+    fn get_data_a(&self) -> &Vec<f64> {
+        &self.data_a
+    }
+
+    fn get_current_a(&self) -> &f64 {
+        let max = self.data_a.len() - 1;
+        &self.data_a[max]
+    }
+
+    fn get_data_b(&self) -> &Vec<f64> {
+        &self.data_b
+    }
+
+    fn get_current_b(&self) -> &f64 {
+        let max = self.data_a.len() - 1;
+        &self.data_a[max]
+    }
+
+    fn get_data_c(&self) -> &Vec<f64> {
+        &self.data_c
+    }
+
+    fn get_current_c(&self) -> &f64 {
+        let max = self.data_c.len() - 1;
+        &self.data_c[max]
+    }
+
+    //FIXME return self to get values from other indicators
+    fn next(&mut self, value: f64) -> Result<()> {
+        let a = self.bb.next(value);
+        let w = (a.upper - a.lower) / a.average;
+        self.data_a.push(w);
+        // self.data_a.push(a.upper);
+        // self.data_b.push(a.lower);
+        // self.data_c.push(a.average);
+        Ok(())
+    }
+    fn next_OHLC(&mut self, OHLC: (f64, f64, f64, f64)) -> Result<()> {
+        Ok(())
+    }
+}
