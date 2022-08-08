4eb84380d1ed1fcb278cd14ed6dbbb804424d038
Pablo <magazpablo@gmail.com>
2022-08-08 13:42:30 +0200

    chore: update deployment

--- rs_algo_backend/src/main.rs
+++ rs_algo_backend/src/main.rs
@@ -53,7 +53,7 @@ async fn main() -> Result<()> {
             .unwrap();
 
     println!(
-        "[BACKEND] Launching {:} on port {:?}",
+        "[BACKEND] 111 Launching {:} on port {:?}",
         app_name,
         port.clone()
     );
